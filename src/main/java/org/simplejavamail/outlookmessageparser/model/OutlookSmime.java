bf4dc2196346c17b43eb38949db95c5451c81f4c
bbottema <b.bottema@projectnibble.org>
2019-05-12 14:25:45 +0200

    Added toString and equals methods to the smime classes

--- src/main/java/org/simplejavamail/outlookmessageparser/model/OutlookSmime.java
+++ src/main/java/org/simplejavamail/outlookmessageparser/model/OutlookSmime.java
@@ -1,5 +1,7 @@
 package org.simplejavamail.outlookmessageparser.model;
 
+import java.util.Objects;
+
 // https://tools.ietf.org/html/rfc5751#page-32 (Identifying an S/MIME Message)
 public abstract class OutlookSmime {
 	
@@ -12,6 +14,31 @@ public abstract class OutlookSmime {
 			this.smimeMime = smimeMime;
 			this.smimeType = smimeType;
 			this.smimeName = smimeName;
+		}
+		
+		@Override
+		public boolean equals(Object o) {
+			if (this == o) return true;
+			if (o == null || getClass() != o.getClass()) return false;
+			OutlookSmimeApplicationSmime that = (OutlookSmimeApplicationSmime) o;
+			return Objects.equals(smimeMime, that.smimeMime) &&
+					Objects.equals(smimeType, that.smimeType) &&
+					Objects.equals(smimeName, that.smimeName);
+		}
+		
+		@Override
+		public int hashCode() {
+			return Objects.hash(smimeMime, smimeType, smimeName);
+		}
+		
+		@Override
+		public String toString() {
+			final StringBuilder sb = new StringBuilder("OutlookSmimeApplicationSmime{");
+			sb.append("smimeMime='").append(smimeMime).append('\'');
+			sb.append(", smimeType='").append(smimeType).append('\'');
+			sb.append(", smimeName='").append(smimeName).append('\'');
+			sb.append('}');
+			return sb.toString();
 		}
 		
 		public String getSmimeMime() { return smimeMime; }
@@ -28,6 +55,31 @@ public abstract class OutlookSmime {
 			this.smimeMime = smimeMime;
 			this.smimeProtocol = smimeProtocol;
 			this.smimeMicalg = smimeMicalg;
+		}
+		
+		@Override
+		public boolean equals(Object o) {
+			if (this == o) return true;
+			if (o == null || getClass() != o.getClass()) return false;
+			OutlookSmimeMultipartSigned that = (OutlookSmimeMultipartSigned) o;
+			return Objects.equals(smimeMime, that.smimeMime) &&
+					Objects.equals(smimeProtocol, that.smimeProtocol) &&
+					Objects.equals(smimeMicalg, that.smimeMicalg);
+		}
+		
+		@Override
+		public int hashCode() {
+			return Objects.hash(smimeMime, smimeProtocol, smimeMicalg);
+		}
+		
+		@Override
+		public String toString() {
+			final StringBuilder sb = new StringBuilder("OutlookSmimeMultipartSigned{");
+			sb.append("smimeMime='").append(smimeMime).append('\'');
+			sb.append(", smimeProtocol='").append(smimeProtocol).append('\'');
+			sb.append(", smimeMicalg='").append(smimeMicalg).append('\'');
+			sb.append('}');
+			return sb.toString();
 		}
 		
 		public String getSmimeMime() { return smimeMime; }
